c15669307e1b09f4b3073a1fd7e723d2bd6100e7
Dmitry Jum <xfiler@ymail.com>
2014-11-06 16:15:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### listSorting
+The application takes arguments with file names to read and write and sorts strings of text in alphabetical and numerical order removing non-numbers and non-letter symbols from words while keeping numbers in the same spots but in ascennding order.
+
+To start the programm run:
+> ruby listSorting.rb inputFile.txt outputFile.txt
+
+Your input file may contain text like:
+> bu% asd 6 n$um 5
+
+And your output file will have:
+
+>asd bu 5 num 6 
+
+Please submit pull requests or send messages if you notice errors after testing.
---
+++ listSorting.rb
@@ -0,0 +1,63 @@
+require 'pry'
+
+class Sorter
+
+  def initialize(input, output)
+    @input = input
+    @output = output
+    @file_read
+    @array
+  end
+
+  def read
+    f = File.open(@input, "r")
+    @file_read = f.read
+    f.close
+  end
+
+  def convert_to_arr
+    # converting string to array removing non-numbers and non-letters
+    @array = @file_read.split(" ").map {|s| s.gsub /\W/, ""}
+  end
+
+  def sort
+    # storing indicies of number strings to map them back later
+    num_indicies = @array.map {|i| @array.index(i) if (i =~ /\d/) == 0}.compact
+    # if there are numbers
+    if num_indicies.empty?
+      @array.sort!
+      #if there are only numbers
+    elsif num_indicies.length == @array.length
+      @array = @array.map {|n| n.to_i}.sort
+    else
+      # separating numbers and words for proper sort
+      separation = @array.partition {|c| (c =~ /\d/) == 0}
+      # sorting first array. Converting to integer in order to sort numbers bigger than 10
+      separation[0] = separation[0].map {|n| n.to_i}.sort
+      # sorting array of words and letters
+      separation[1].sort!
+      # inserting numbers in their original spots
+      index = 0
+      separation[0].each do |num|
+        #inserting and sorted integers inside of array of sorted strings, simultaniously converting them into strings
+        @array = separation[1].insert(num_indicies[index], num.to_s)
+        # switching index for another iteration
+        index += 1
+      end
+    end
+  end
+
+  def write
+    f = File.new(@output, "w")
+    f.write @array.join(" ")
+  end
+
+  def exe
+    self.read
+    self.convert_to_arr
+    self.sort
+    self.write
+  end
+end
+
+Sorter.new(ARGV[0], ARGV[1]).exe
